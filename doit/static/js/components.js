e803e039383d568afe29243f7d2005da9657802c
Marius Pana <mp@spearhead.systems>
2019-11-24 11:48:48 +0200

    vue for components on dashboard

---
+++ doit/static/js/components.js
@@ -0,0 +1,377 @@
+Vue.component('all-open-incidents-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/allopenincidents/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default  uk-card-body v-cloak"> <p class="uk-text-center">All Incidents</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#incidents" onclick="UIkit.accordion(`#dashboard-accordion`).toggle(0);" uk-scroll>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-sun.png" > </div>'
+});
+
+var allopenincidents = new Vue({
+	el: '#all-open-incidents',
+});
+
+Vue.component('my-open-incidents-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/myopenincidents/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default  uk-card-body v-cloak"> <p class="uk-text-center">My Incidents</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#myincidents-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-sun.png"> </div>'
+});
+
+var myopenincidents = new Vue({
+	el: '#my-open-incidents',
+});
+
+Vue.component('my-backlog-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/mybacklog/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default  uk-card-body v-cloak"> <p class="uk-text-center">My Backlog</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#mybacklog-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-checklist.png"> </div>'
+});
+
+var mybacklog = new Vue({
+	el: '#my-backlog-cards',
+});
+
+Vue.component('all-backlog-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/allbacklog/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default  uk-card-body v-cloak"> <p class="uk-text-center">All Backlog</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#allbacklog-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-checklist.png"> </div>'
+});
+
+var mybacklog = new Vue({
+	el: '#all-backlog-cards',
+});
+
+Vue.component('my-overdue-cards-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/myoverduecards/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.cardslist = response.data;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default  uk-card-body v-cloak"> <p class="uk-text-center">My Overdue Cards</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#myoverduecards-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-beach.png"> </div>'
+});
+
+var myoverduecards = new Vue({
+	el: '#my-overdue-cards',
+});
+
+Vue.component('my-open-cards-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/mycards/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default uk-card-body v-cloak"> <p class="uk-text-center">My Cards</p> <h1 v-if="cards" class="uk-text-warning uk-text-center"> <a class="uk-text-warning" href="#mycards-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-facebook_like.png"> </div>'
+});
+
+var myopencards = new Vue({
+	el: '#my-open-cards',
+});
+
+Vue.component('my-overdue-boards-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			boards: []
+		}
+	},
+	mounted: function() {
+		this.getBoards();
+	},
+	methods: {
+		getBoards: function() {
+			this.loading = true;
+			this.$http.get('/api2/myoverdueboards/')
+				.then((response) => {
+					this.boards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getBoards();
+		setInterval(function () {
+			this.getBoards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default uk-card-body v-cloak"> <p class="uk-text-center">My Overdue Boards</p> <h1 v-if="boards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#myoverdueboard-modal" uk-toggle>[[boards]]</a> </h1> <img v-else src="/static/img/icons/icons8-checkmark.png"> </div>'
+});
+
+var myooverdueboards = new Vue({
+	el: '#my-overdue-boards',
+});
+
+Vue.component('cards-without-owner-or-company-component',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/noownerorcompany/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default uk-card-body v-cloak"> <p class="uk-text-center">No Owner or Company</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#noowner-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-ok_hand.png"> </div>'
+});
+
+var noownerorcompany = new Vue({
+	el: '#no-owner-or-company',
+});
+
+Vue.component('cards-im-watching',{
+	delimiters: ['[[', ']]'],
+	props: '',
+	data: function () {
+		return {
+			cards: []
+		}
+	},
+	mounted: function() {
+		this.getCards();
+	},
+	methods: {
+		getCards: function() {
+			this.loading = true;
+			this.$http.get('/api2/cardswatcher/')
+				.then((response) => {
+					this.cards = response.data.length;
+					this.loading = false;
+				})
+				.catch((err) => {
+				 this.loading = false;
+				 //console.log(err);
+				})
+		},
+	},
+	created: function () {
+		this.getCards();
+
+		setInterval(function () {
+			this.getCards();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	template: '<div class="uk-card-small uk-card-default uk-card-body" v-cloak> <p class="uk-text-center">Cards I\'m Watching</p> <h1 v-if="cards" class="uk-text-danger uk-text-center"> <a class="uk-text-danger" href="#cardswatcher-modal" uk-toggle>[[cards]]</a> </h1> <img v-else src="/static/img/icons/icons8-cafe.png"> </div>'
+});
+
+var cardswatcher = new Vue({
+	el: '#cards-watcher',
+});
--- doit/static/js/vue-dashboard.js
+++ doit/static/js/vue-dashboard.js
@@ -1,55 +1,82 @@
+// make sure we send csrf django token
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+Vue.http.headers.common['X-CSRF-TOKEN'] = csrftoken;
+Vue.http.headers.common['X-CSRFTOKEN'] = csrftoken;
 
 // my_incidents
 var my_incidents = new Vue({
 	delimiters: ['[[', ']]'],
-	el: '#my_incidents',
+	el: '#my-incidents',
 	data: {
 		my_incidents: ''
 	},
 	methods: {
 		loadData: function () {
-			$.get('/cards/my_incidents', function (response) {
-				this.my_incidents = response.my_incidents;
+			$.get('/api2/myopenincidents', function (response) {
+				this.my_incidents = response;
 			}.bind(this));
-		}
+		},
+		moment: function () {
+    	return moment();
+  	}
 	},
 	created: function () {
 		this.loadData();
-
 		setInterval(function () {
 			this.loadData();
 		}.bind(this), 20000);
 	},
 	beforeDestroy: function(){
 		clearInterval(this.interval);
+	},
+	filters: {
+  	moment: function (date) {
+    	return moment(date).format('MMM Do YYYY, k:m');
+		}
 	}
 })
 
 // my_cards
-var my_open = new Vue({
-	delimiters: ['[[', ']]'],
-	el: '#my_vue_cards',
-	data: {
-		my_open_cards: ''
-	},
-	methods: {
-		loadData: function () {
-			$.get('/my_vue_cards', function (response) {
-				this.my_open_cards = response.open_cards;
-			}.bind(this));
-		}
-	},
-	created: function () {
-		this.loadData();
-
-		setInterval(function () {
-			this.loadData();
-		}.bind(this), 20000);
-	},
-	beforeDestroy: function(){
-		clearInterval(this.interval);
-	}
-})
+//var my_open = new Vue({
+//	delimiters: ['[[', ']]'],
+//	el: '#my_vue_cards',
+//	data: {
+//		my_open_cards: ''
+//	},
+//	methods: {
+//		loadData: function () {
+//			$.get('/my_vue_cards', function (response) {
+//				this.my_open_cards = response.open_cards;
+//			}.bind(this));
+//		}
+//	},
+//	created: function () {
+//		this.loadData();
+//
+//		setInterval(function () {
+//			this.loadData();
+//		}.bind(this), 20000);
+//	},
+//	beforeDestroy: function(){
+//		clearInterval(this.interval);
+//	}
+//})
 
 //all_my_open_cards
 var all_my_open_cards = new Vue({
@@ -62,8 +89,19 @@ var all_my_open_cards = new Vue({
 		loadData: function () {
 			$.get('/cards/all_my_open_cards', function (response) {
 				this.all_my_open_cards = response.allmycards;
-				console.log(this);
 			}.bind(this));
+		},
+		deleteCard: function(id) {
+			this.loading = true;
+			this.$http.delete(`/api2/mycards/${[[id]]}/`)
+				.then((response) => {
+					this.loading = false;
+					this.loadData();
+				})
+				.catch((err) => {
+					this.loading = false;
+					console.log(err);
+				})
 		},
 		moment: function () {
     	return moment();
@@ -82,8 +120,100 @@ var all_my_open_cards = new Vue({
 	filters: {
   	moment: function (date) {
     	return moment(date).format('MMM Do YYYY, k:m');
-  		}
 		}
+	}
+})
+
+//my_backlogs
+var my_backlog = new Vue({
+	delimiters: ['[[', ']]'],
+	el: '#my-backlog',
+	data: {
+		cards: ''
+	},
+	methods: {
+		loadData: function () {
+			$.get('/api2/mybacklog/', function (response) {
+				this.cards = response;
+			}.bind(this));
+		},
+		deleteCard: function(id) {
+			this.loading = true;
+			this.$http.delete(`/api2/mybacklog/${[[id]]}/`)
+				.then((response) => {
+					this.loading = false;
+					this.loadData();
+				})
+				.catch((err) => {
+					this.loading = false;
+					console.log(err);
+				})
+		},
+		moment: function () {
+    	return moment();
+  	}
+	},
+	created: function () {
+		this.loadData();
+
+		setInterval(function () {
+			this.loadData();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	filters: {
+  	moment: function (date) {
+    	return moment(date).format('MMM Do YYYY, k:m');
+		}
+	}
+})
+
+//all_backlogs
+var my_backlog = new Vue({
+	delimiters: ['[[', ']]'],
+	el: '#all-backlog',
+	data: {
+		cards: ''
+	},
+	methods: {
+		loadData: function () {
+			$.get('/api2/allbacklog/', function (response) {
+				this.cards = response;
+			}.bind(this));
+		},
+		deleteCard: function(id) {
+			this.loading = true;
+			this.$http.delete(`/api2/allbacklog/${[[id]]}/`)
+				.then((response) => {
+					this.loading = false;
+					this.loadData();
+				})
+				.catch((err) => {
+					this.loading = false;
+					console.log(err);
+				})
+		},
+		moment: function () {
+    	return moment();
+  	}
+	},
+	created: function () {
+		this.loadData();
+
+		setInterval(function () {
+			this.loadData();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	filters: {
+  	moment: function (date) {
+    	return moment(date).format('MMM Do YYYY, k:m');
+		}
+	}
 })
 
 // my_overdue_cards_list
@@ -91,12 +221,62 @@ var my_overdue_cards_list = new Vue({
 	delimiters: ['[[', ']]'],
 	el: '#my_overdue_cards_list',
 	data: {
-		my_overdue_cards_list: ''
+		my_overdue_cards_list: '',
+		loading: false,
+		currentCard: {},
 	},
 	methods: {
 		loadData: function () {
 			$.get('/cards/my_overdue_cards_list', function (response) {
 				this.my_overdue_cards_list = response.my_overdue_cards_list;
+			}.bind(this));
+		},
+		deleteCard: function(id) {
+			this.loading = true;
+			this.$http.delete(`/api2/cards/${[[id]]}/`)
+				.then((response) => {
+					this.loading = false;
+					this.loadData();
+				})
+				.catch((err) => {
+					this.loading = false;
+					console.log(err);
+				})
+		},
+		moment: function () {
+    	return moment();
+  	}
+	},
+	created: function () {
+		this.loadData();
+
+		setInterval(function () {
+			this.loadData();
+		}.bind(this), 20000);
+	},
+	beforeDestroy: function(){
+		clearInterval(this.interval);
+	},
+	filters: {
+  	moment: function (date) {
+    	return moment(date).format('MMM Do YYYY, k:m');
+  		}
+		}
+})
+
+// my_overdue_board_list
+var my_overdue_boards_list = new Vue({
+	delimiters: ['[[', ']]'],
+	el: '#my_overdue_boards_list',
+	data: {
+		my_overdue_boards_list: '',
+		loading: false,
+		currentCard: {},
+	},
+	methods: {
+		loadData: function () {
+			$.get('/api2/myoverdueboards/', function (response) {
+				this.my_overdue_boards_list = response;
 			}.bind(this));
 		},
 		moment: function () {
@@ -108,7 +288,7 @@ var my_overdue_cards_list = new Vue({
 
 		setInterval(function () {
 			this.loadData();
-		}.bind(this), 2000);
+		}.bind(this), 20000);
 	},
 	beforeDestroy: function(){
 		clearInterval(this.interval);
@@ -119,45 +299,61 @@ var my_overdue_cards_list = new Vue({
   		}
 		}
 })
-// my_overdue_cards
-var my_overdue = new Vue({
-	delimiters: ['[[', ']]'],
-	el: '#my_vue_overdue',
-	data: {
-		my_overdue_cards: ''
-	},
-	methods: {
-		loadData: function () {
-			$.get('/my_vue_overdue', function (response) {
-				this.my_overdue_cards = response.overdue_cards;
-			}.bind(this));
-		}
-	},
-	created: function () {
-		this.loadData();
 
-		setInterval(function () {
-			this.loadData();
-		}.bind(this), 20000);
-	},
-	beforeDestroy: function(){
-		clearInterval(this.interval);
-	}
-})
+//// my_overdue_cards
+//var my_overdue = new Vue({
+//	delimiters: ['[[', ']]'],
+//	el: '#my_vue_overdue',
+//	data: {
+//		my_overdue_cards: ''
+//	},
+//	methods: {
+//		loadData: function () {
+//			$.get('/my_vue_overdue', function (response) {
+//				this.my_overdue_cards = response.overdue_cards;
+//			}.bind(this));
+//		}
+//	},
+//	created: function () {
+//		this.loadData();
+//
+//		setInterval(function () {
+//			this.loadData();
+//		}.bind(this), 20000);
+//	},
+//	beforeDestroy: function(){
+//		clearInterval(this.interval);
+//	}
+//})
 
 // cards without owner
-var cards_without_owner = new Vue({
+var cards_without_owner_or_company = new Vue({
 	delimiters: ['[[', ']]'],
 	el: '#cards_without_owner',
-	data: {
-		cards_without_owner: ''
+	data: function () {
+		return {
+			no_owner_or_company: []
+		}
 	},
 	methods: {
 		loadData: function () {
-			$.get('/cards/cards_without_owner', function (response) {
-				this.cards_without_owner = response.cards_without_owner;
+			$.get('/api2/noownerorcompany', function (response) {
+				this.no_owner_or_company = response;
+				console.log(response.data);
 			}.bind(this));
-		}
+		},
+	deleteCard: function(id) {
+			this.loading = true;
+			this.$http.delete(`/api2/cards/${[[id]]}/`)
+				.then((response) => {
+					this.loading = false;
+					this.loadData();
+				})
+				.catch((err) => {
+					this.loading = false;
+					console.log(err);
+				})
+		},
 	},
 	created: function () {
 		this.loadData();
@@ -168,23 +364,34 @@ var cards_without_owner = new Vue({
 	},
 	beforeDestroy: function(){
 		clearInterval(this.interval);
+	},
+	filters: {
+	moment: function (date) {
+		return moment(date).format('MMM Do YYYY, k:m');
+		}
 	}
 })
 
 
-//  cards_without_company
-var cards_without_company = new Vue({
+// cards im watching
+var cards_watcher = new Vue({
 	delimiters: ['[[', ']]'],
-	el: '#cards_without_company',
-	data: {
-		cards_without_company: ''
+	el: '#cards_watcher',
+	data: function () {
+		return {
+			cards: []
+		}
 	},
 	methods: {
 		loadData: function () {
-			$.get('/cards/cards_without_company', function (response) {
-				this.cards_without_company = response.cards_without_company;
+			$.get('/api2/cardswatcher', function (response) {
+				this.cards = response;
+				console.log(response.data);
 			}.bind(this));
-		}
+		},
+		moment: function () {
+    	return moment();
+  	}
 	},
 	created: function () {
 		this.loadData();
@@ -195,5 +402,90 @@ var cards_without_company = new Vue({
 	},
 	beforeDestroy: function(){
 		clearInterval(this.interval);
+	},
+	filters: {
+	moment: function (date) {
+		return moment(date).format('MMM Do YYYY, k:m');
+		}
 	}
 })
+
+
+// test rest
+//var allcards  = new Vue({
+//   el: '#starting',
+//   delimiters: ['[[', ']]'],
+//   data: {
+//		 cards: [],
+//		 loading: false,
+//		 currentCard: {},
+//		 message: null,
+//		 newCard: { 'title': null, 'description': null },
+// },
+// mounted: function() {
+//	this.getCards();
+//},
+// methods: {
+//	getCards: function() {
+//		this.loading = true;
+//		this.$http.get('/api2/cards/')
+//      .then((response) => {
+//        this.cards = response.data;
+//        this.loading = false;
+//      })
+//      .catch((err) => {
+//       this.loading = false;
+//       console.log(err);
+//      })
+// },
+// 	getCard: function(id) {
+//		this.loading = true;
+//		this.$http.get('/api2/cards/[[id]]/')
+//      .then((response) => {
+//        this.currentArticle = response.data;
+//        this.loading = false;
+//      })
+//      .catch((err) => {
+//        this.loading = false;
+//        console.log(err);
+//      })
+// },
+// addArticle: function() {
+//  this.loading = true;
+//  this.$http.post('/api/article/',this.newArticle)
+//      .then((response) => {
+//        this.loading = false;
+//        this.getArticles();
+//      })
+//      .catch((err) => {
+//        this.loading = false;
+//        console.log(err);
+//      })
+// },
+// updateCard: function() {
+//  this.loading = true;
+//  this.$http.put(`/api2/cards/${this.currentCard.id}/`,     this.currentCard)
+//      .then((response) => {
+//        this.loading = false;
+//        this.currentCard = response.data;
+//        this.getCards();
+//      })
+//      .catch((err) => {
+//        this.loading = false;
+//        console.log(err);
+//      })
+// },
+// deleteCard: function(id) {
+//  this.loading = true;
+//  this.$http.delete(`/api2/cards/${[[id]]}/`)
+//      .then((response) => {
+//        this.loading = false;
+//        this.getCards();
+//      })
+//      .catch((err) => {
+//        this.loading = false;
+//        console.log(err);
+//      })
+//	}
+//	}
+//})
