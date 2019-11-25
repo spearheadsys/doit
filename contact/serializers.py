174a9668e2a70657cf9fdce36761e95690390781
Marius Pana <mp@spearhead.systems>
2019-11-25 21:47:57 +0200

    fix typo

--- contact/serializers.py
+++ contact/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (__all__)
+        fields = '__all__'
