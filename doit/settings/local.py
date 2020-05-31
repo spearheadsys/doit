fc6007572bcade0f4310df133a0a2e03caf99628
Marius Pana <mp@spearhead.systems>
2020-05-31 12:03:51 +0300

    cleanup, add site_url to per env config file

--- doit/settings/local.py
+++ doit/settings/local.py
@@ -1,11 +1,9 @@
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from .base import *
 
 SITE_URL = "http://127.0.0.1:8000/"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-# Application definition
 INSTALLED_APPS += (
     # 'debug_toolbar',
 )
@@ -19,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'doit',
         'USER': 'root',
-        'PASSWORD': 'marius',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -27,20 +25,3 @@ DATABASES = {
 
 EMAIL_HOST = 'localhost'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#         }
-#     },
-#     'loggers': {
-#         'django.db.backends': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#         },
-#     }
-# }
