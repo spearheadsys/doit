71718840bad380f057ad601b9409ced05a7f2c76
Marius Pana <mp@spearhead.systems>
2020-06-11 19:03:31 +0300

    add report static js and functions (per app)

---
+++ report/static/report/js/reportapp.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  console.log("DoIT :: loading reportapp.js")
+
+  var report = $('#reports').DataTable({
+    buttons: [
+        'copy', 'csv'
+    ]
+  });
+  report.buttons().container().appendTo( $('.col-sm-6:eq(0)', report.table().container() ) );
+
+
+  $(function() {
+    $('input[name="daterange"]').daterangepicker(
+    {
+      timePicker: true,
+      timePicker24Hour: true,
+      locale: {
+          format: 'YYYY-MM-DD H:mm'
+      },
+      ranges: {
+       'Today': [moment(), moment()],
+       'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+       'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+       'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+       'This Month': [moment().startOf('month'), moment().endOf('month')],
+       'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      }
+    }
+    );
+  });
+
+  console.log("DoIT :: loaded reportapp.js")
+});
