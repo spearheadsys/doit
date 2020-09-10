ef7cb945895e71cb534a4f945a4f2ee8b5896e27
abarbu <ab@spearhead.systems>
2020-09-10 12:42:28 +0300

    added model admin search for UserProfile

--- contact/admin.py
+++ contact/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from contact.models import UserProfile
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ['user__email', 'user__first_name', 'user__last_name']
+
+
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
