163c7231e5836df65fafa076971911221d420e79
Marius Pana <mp@spearhead.systems>
2020-10-28 19:33:20 +0200

    remove tasks/reminders

--- card/admin.py
+++ card/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from card.models import Card, Worklog, Priority, Column, Task, Columntype, Reminder
+from card.models import Card, Worklog, Priority, Column, Columntype
 from django.contrib.auth.models import User
 from contact.models import UserProfile
-
-
-# class ProfileInline(admin.StackedInline):
-# 	model = UserProfile
-# 	can_delete = False
-# 	verbose_name_plural = 'profile'
-
 
 class CardAdmin(admin.ModelAdmin):
     model = Card
@@ -23,19 +16,8 @@ class ColumnAdmin(admin.ModelAdmin):
 class ReminderAdmin(admin.ModelAdmin):
     list_display = ('reminder_time', 'card', 'owner')
 
-
-# Define a new User admin
-# class UserAdmin(admin.ModelAdmin):
-# 	inlines = (ProfileInline, )
-
 admin.site.register(Card, CardAdmin)
 admin.site.register(Column, ColumnAdmin)
 admin.site.register(Worklog)
 admin.site.register(Priority)
-admin.site.register(Task)
 admin.site.register(Columntype)
-admin.site.register(Reminder, ReminderAdmin)
-
-# admin.site.unregister(User)
-# admin.site.register(User)
-# +, UserAdmin
