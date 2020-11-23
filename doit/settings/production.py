8b48b6e91308276abbeb68932022765747b8196d
Marius Pana <mp@spearhead.systems>
2020-11-23 19:53:29 +0200

    enable redis cache on localhost

--- doit/settings/production.py
+++ doit/settings/production.py
@@ -16,4 +16,21 @@ DATABASES = {
     }
 }
 
+
+CACHES = {
+    # 'default': {
+    #     'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    #     'LOCATION': 'unique-snowflake',
+    # }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "doit"
+    }
+
+}
+
 USE_X_FORWARDED_HOST = True
