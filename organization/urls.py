5f37c1fe8d24feb46401ead86db09910df70e799
Marius Pana <mp@spearhead.systems>
2020-11-26 07:59:06 +0200

    remove emailsdomains from urls

--- organization/urls.py
+++ organization/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from organization.views import organizations, add_organization, get_company, \
-    EmailDomainAutocomplete, delete_organization
+    delete_organization
     #, knowledge_base
 
 urlpatterns = [
@@ -9,9 +9,4 @@ urlpatterns = [
     url(r'^get_company/', get_company, name='get_company'),
     url(r'^delete_organization/(?P<company>\d+)', delete_organization, name='delete_organization'),
     #url(r'^knowledge_base/(?P<company>\d+)', knowledge_base, name='knowledge_base'),
-    url(
-        r'^emaildomain-autocomplete/$',
-        EmailDomainAutocomplete.as_view(create_field='domain'),
-        name='emaildomain-autocomplete',
-    ),
 ]
