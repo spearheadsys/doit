273be46bad14bf27ffcf765f10e9bb013392ebef
Marius Pana <mp@spearhead.systems>
2020-11-26 07:56:03 +0200

    update org admin.py with kb

--- organization/admin.py
+++ organization/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from contact.models import UserProfile
+from organization.models import Organization, KnowledgeBase, \
+    KnowledgeBaseArticle, KnowledgeBaseCategory
 
-from organization.models import Organization, EmailDomain
+
+class KBInline(admin.TabularInline):
+    model = KnowledgeBase
+    extra = 1
 
 
-# Register your models here.
-admin.site.register(Organization)
-admin.site.register(EmailDomain)
+class ContactInline(admin.TabularInline):
+    model = UserProfile
+    extra = 1
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    model = Organization
+    list_display = ('name', 'active', 'created_time')
+    search_fields = ['name']
+    inlines = [
+        KBInline,
+        ContactInline
+    ]
+
+    class Meta:
+        ordering = ['created_time']
+
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(KnowledgeBase)
+admin.site.register(KnowledgeBaseCategory)
+admin.site.register(KnowledgeBaseArticle)
 
